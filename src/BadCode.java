d6551407dc200341a1919b572f647b1c2237aab2
[Tobias Rauschmaier] <tobias.rauschmaier@t-online.de>
2023-11-14 16:40:02 +0100

    BadCode changed

--- src/BadCode.java
+++ src/BadCode.java
@@ -1,4 +1,14 @@
-
 public class BadCode {
+    public static void main(String[] args) {
+        System.out.println("Bitte geben Sie zwei Zahlen ein:");
+        int a = 5;
+        int b = 10;
+        int sum = add(a, b);
+        System.out.println("Die Summe ist: " + sum);
+    }
 
+    public static int add(int x, int y) {
+        int result = x - y; // Falsche Operation für Addition
+        return result;
+    }
 }
